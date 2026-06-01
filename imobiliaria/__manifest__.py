5d0425e24e43fb66fc80685849d03e449e7f8e17
margareth.tanno <margareth.tanno@gmail.com>
2026-06-01 16:10:16 -0300

    Criada estrutura inicial do módulo imobiliaria

---
+++ imobiliaria/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Imobiliaria',
+    'version': '1.0',
+    'summary': 'Gestao de Imoveis e Locacoes',
+    'category': 'Real Estate',
+    'author': 'Margareth Tanno',
+    'depends': ['base'],
+    'installable': True,
+    'application': True,
+}
